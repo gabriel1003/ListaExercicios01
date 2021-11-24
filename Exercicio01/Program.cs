bf0d15eeeb38cafd48815de644ac5f447c546908
Gabriel Merconi <merconigabriel22@gmail.com>
2021-11-24 09:38:38 -0300

    códigos do curso de c#

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System;
+
+namespace Exercicio01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+     {
+Console.WriteLine("digite a primeira nota: ");
+string? textoNota01 = Console.ReadLine();
+Console.WriteLine("digite a segunda nota: ");
+string? textoNota02 = Console.ReadLine();
+double nota01 = Convert.ToDouble(textoNota01);
+double nota02 = Convert.ToDouble(textoNota02);
+double media = (nota01 + nota02) / 2;
+if (media == 10)
+{
+    Console.WriteLine("você foi aprovado com distinção");
+}
+else if (media >= 7)
+{
+    Console.WriteLine("você foi aprovado. ");
+}
+else
+{
+    Console.WriteLine("você foi reprovado. ");
+}
+        }
+    }
+}
---
+++ Exercicio02/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("digite um número: ");
+            string? textoNumero01 = Console.ReadLine();
+            Console.WriteLine("digite outro número: ");
+            string? textoNumero02 = Console.ReadLine();
+            Console.WriteLine("digite o ultimo número: ");
+            string? textoNumero03 = Console.ReadLine();
+            int numero01 = Convert.ToInt32(textoNumero01);
+            int numero02 = Convert.ToInt32(textoNumero02);
+            int numero03 = Convert.ToInt32(textoNumero03);
+            int maior = 0;
+            int menor =0;
+            if (numero01 > numero02)
+            {
+                if (numero01 > numero03)
+                {
+                    maior = numero01;
+                }
+                else
+                {
+                    maior = numero03;
+                }
+            }
+            else
+            {
+                if (numero02 > numero03)
+                {
+                    maior = numero02;
+                }
+                else
+                {
+                    maior = numero03;
+                }
+            }
+
+            if (numero01 < numero02)
+            {
+                if (numero01 < numero03)
+                {
+                    menor = numero01;
+                }
+                else
+                {
+                    menor = numero03;
+                }
+            }
+            else
+            {
+                if (numero02 < numero03)
+                {
+                    menor = numero02;
+                }
+                else
+                {
+                    menor = numero03;
+                    
+                }
+            }
+            Console.WriteLine("o maior número é: " +maior);
+                    Console.WriteLine("o menor número é:" +menor);
+        }
+    }
+}
